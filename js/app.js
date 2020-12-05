5add90e2237db16d4e647be86e7a3976bce2b2f9
Aimee Armour-Avant <aimeeavant@gmail.com>
2020-12-05 14:04:05 -0800

    :(

--- js/app.js
+++ js/app.js
@@ -2,16 +2,23 @@
 
 // Create variable to hold initial option.
 let options = {
-    center: [35.5528, -120.8266],
-    zoom: 8,
+    center: [35.3102, -120.4358],
+    zoom: 4,
     zoomSnap: .1,
 }
-// Create variable to hold the map
-let map = L.map('map', options);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+// Create variable to hold the map
+var map = L.map('map', options);
+
+
+var basemap_url = 'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png'
+
+var basemap_attributes = {
+    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
+    maxZoom: 17
+}
+
+var tiles = L.tileLayer(basemap_url, basemap_attributes).addTo(map);
 
 
 // add scale bar
@@ -34,7 +41,7 @@ var countyLayer = jQuery.getJSON("data/sloCountyBoundary.geojson", function (dat
         }
     }).addTo(map);
     // can call to new function here gets the data out of the callback function  
-    map.fitBounds(countyLayer.getBounds())
+   // map.fitBounds(countyLayer.getBounds())
 
 });// end of callback function
 
